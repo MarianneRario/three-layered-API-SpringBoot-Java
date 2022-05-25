ae7989db7220813fd3e1553852151733b0f62a91
Marianne Rario <mariannecastillorario27@gmail.com>
2022-05-25 23:34:57 +0800

    create student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	// create a test method
+	@GetMapping // get something out for our server
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Marianne",
+						22,
+						LocalDate.of(1999, Month.OCTOBER, 7),
+						"marianne.rario@my.jru.edu"));
 	}
 
 }
