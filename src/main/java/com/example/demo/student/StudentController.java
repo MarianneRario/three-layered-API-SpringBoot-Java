e7f8e7b9a8b0c24fa4b54a8f37840a16bc7e1e38
Marianne Rario <mariannecastillorario27@gmail.com>
2022-05-26 00:03:14 +0800

    instantiated studentservice class by adding component and autowired annotations

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class StudentController {
     // create a reference of method in StudentService.java
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
