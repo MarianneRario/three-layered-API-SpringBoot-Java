550e909aeba5dd393bc32cfd82b8bae1eb42347e
Marianne Rario <mariannecastillorario27@gmail.com>
2022-05-26 13:59:13 +0800

    create a student config file that contains a bean

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    // create a bean
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student marianne = new Student(
+                    1L,
+                    "Marianne",
+                    22,
+                    LocalDate.of(1999, Month.OCTOBER, 7),
+                    "marianne@gmail.com"
+            );
+
+            Student paulo = new Student(
+                    "Paulo",
+                    22,
+                    LocalDate.of(1999, Month.JULY, 2),
+                    "paulo@gmail.com"
+            );
+
+            // save these students to the database
+            studentRepository.saveAll(List.of(marianne, paulo)); // takes a list
+        };
+    }
+
+}
