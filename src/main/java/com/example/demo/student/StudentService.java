7a336f3f65dd4fccdee3d7845f16c2d5bee12f40
Marianne Rario <mariannecastillorario27@gmail.com>
2022-05-26 13:21:15 +0800

    implemented student repository interface

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,13 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Marianne",
-                        22,
-                        LocalDate.of(1999, Month.OCTOBER, 7),
-                        "marianne@mapletreemedia.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
